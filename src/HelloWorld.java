1dc9bf76ad582a99186c4c0763850991c23de33d
Justin Gross <jmbg144@gmail.com>
2017-05-24 13:25:17 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	/* this is a multiline comment
+	 * author: Justin Gross
+	 * date: 05/24/17
+	 * This is hello world
+	 */
+
+	public static void main(String[] args) {
+		// This is an in line comment
+		System.out.println("Hello World!");
+
+	}
+
+}
